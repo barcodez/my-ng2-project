70606cc0cbbeca671f0d349dfe73a13bfae37dca
Rob Young <ryoung@mbopartners.com>
2016-06-05 18:16:34 -0400

    Adding selectability.

--- app/app.component.ts
+++ app/app.component.ts
@@ -65,21 +65,25 @@ styles:[`
 		    <span class="badge">{{hero.id}}</span> {{hero.name}}
 		  </li>
 		</ul>
-		<h2>{{hero.name}} details!</h2>
-		<div><label>id: </label>{{hero.id}}</div>
-		<div>
-			<label>name: </label>
-			<input [(ngModel)]="hero.name" placeholder="name">
+		<div *ngIf="selectedHero">
+			<h2>{{selectedHero.name}} details!</h2>
+			<div><label>id: </label>{{selectedHero.id}}</div>
+			<div>
+				<label>name: </label>
+				<input [(ngModel)]="selectedHero.name" placeholder="name">
+			</div>
 		</div>
 	`
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 11,
-	name: 'Roseabell Tricker'
-  };
+ //  hero: Hero = {
+ //    id: 11,
+	// name: 'Roseabell Tricker'
+ //  };
   public heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero) { this.selectedHero = hero; }
 };
 
 var HEROES: Hero[] = [
