76cc5f2f5577fa8bfc14730495daeaf81d8d8771
Rob Young <ryoung@mbopartners.com>
2016-06-05 17:56:41 -0400

    Adding additional heroes.

--- app/app.component.ts
+++ app/app.component.ts
@@ -20,8 +20,21 @@ export class Hero {
 export class AppComponent {
   title = 'Tour of Heroes';
   hero: Hero = {
-    id: 1,
+    id: 11,
 	name: 'Roseabell Tricker'
-  }
+  };
+  public heroes = HEROES;
 };
 
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Roseabell Tricker" },
+  { "id": 12, "name": "Mr. Mister" },
+  { "id": 13, "name": "Bluebell Bell" },
+  { "id": 14, "name": "Bummo" },
+  { "id": 15, "name": "Mommy-rific" },
+  { "id": 16, "name": "Magenta E-Lee" },
+  { "id": 17, "name": "Wonder Ainsley Girl" },
+  { "id": 18, "name": "Rainbowiss Tricker" },
+  { "id": 19, "name": "Bat-girl" },
+  { "id": 20, "name": "Twister-Guy" },
+];
