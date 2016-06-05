eb3765549cabcffce8b7f876018c2160a2ae506c
Rob Young <ryoung@mbopartners.com>
2016-06-05 18:19:32 -0400

    Adding selected item css.

--- app/app.component.ts
+++ app/app.component.ts
@@ -60,7 +60,9 @@ styles:[`
 		<h1>{{title}}</h1>
 		<h2>My Heroes</h2>
 		<ul class="heroes">
-		  <li *ngFor="let hero of heroes">
+		  <li *ngFor="let hero of heroes" 
+		  	[class.selected]="hero === selectedHero"
+		  	(click)="onSelect(hero)">
 		    <!-- each hero goes here -->
 		    <span class="badge">{{hero.id}}</span> {{hero.name}}
 		  </li>
