19715c28ce13d11bf7abb9cf828c5187f313cfa7
Rob Young <ryoung@mbopartners.com>
2016-06-07 16:53:44 -0400

    Added a route to /heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component }       from '@angular/core';
+import { Component }        from '@angular/core';
+import { RouteConfig,
+         ROUTER_DIRECTIVES,
+         ROUTER_PROVIDERS } from '@angular/router-deprecated'
 
-import { HeroService }     from './hero.service';
-import { HeroesComponent } from './heroes.component';
+import { HeroesComponent}   from './heroes.component';
+import { HeroService }      from './hero.service';
 
 @Component({
 	selector: 'my-app',
 	template: `
-		<h1>{{title}}</h1>
-		<my-heroes></my-heroes>
+      <h1>{{title}}</h1>
+      <a [routerLink]="['Heroes']">Heroes</a>
+      <router-outlet></router-outlet>
 	`,
-	directives: [HeroesComponent],
-	providers: [HeroService]
+	directives: [ROUTER_DIRECTIVES],
+	providers: [ROUTER_PROVIDERS, HeroService]
 })
+@RouteConfig([
+   {
+      path: '/heroes',
+      name: 'Heroes',
+      component: HeroesComponent
+   }
+])
 export class AppComponent {
-	title: string = 'Tour of Heroes';
+   title = 'Tour of Heroes';
 }
