b61acd3d507a0e4b2bc657f90197d2f224bb0af6
Rob Young <ryoung@mbopartners.com>
2016-06-07 00:00:07 -0400

    cleaning up. formatting.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
 @Component({
-styles:[`
+	selector: 'my-app',
+	template: `
+		<h1>{{title}}</h1>
+		<h2>My Heroes</h2>
+		<ul class="heroes">
+		  <li *ngFor="let hero of heroes" 
+		  	[class.selected]="hero === selectedHero"
+		  	(click)="onSelect(hero)">
+		    <!-- each hero goes here -->
+		    <span class="badge">{{hero.id}}</span> {{hero.name}}
+		  </li>
+		</ul>
+		<my-hero-detail [hero]="selectedHero"></my-hero-detail>
+	`,
+	styles:[`
 	  .selected {
 	    background-color: #CFD8DC !important;
 	    color: white;
@@ -54,36 +68,26 @@ styles:[`
 	    border-radius: 4px 0 0 4px;
 	  }
 	`],
-	selector: 'my-app',
-	template: `
-		<h1>{{title}}</h1>
-		<h2>My Heroes</h2>
-		<ul class="heroes">
-		  <li *ngFor="let hero of heroes" 
-		  	[class.selected]="hero === selectedHero"
-		  	(click)="onSelect(hero)">
-		    <!-- each hero goes here -->
-		    <span class="badge">{{hero.id}}</span> {{hero.name}}
-		  </li>
-		</ul>
-		<my-hero-detail [hero]="selectedHero"></my-hero-detail>
-	`,
 	directives: [HeroDetailComponent],
 	providers: [HeroService]
 })
 export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  public heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
-  ngOnInit() {
-	this.getHeroes();  	
-  }
-  onSelect(hero: Hero) { this.selectedHero = hero; }
+
   getHeroes() {
   	this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
+
+  ngOnInit() {
+	this.getHeroes();  	
+  }
+ 
+  onSelect(hero: Hero) { 
+  	this.selectedHero = hero; 
+  }
 };
 
-var HEROES: Hero[];
