e213661c376bd1bbf7f3b16129e600550e9119fa
Rob Young <ryoung@mbopartners.com>
2016-06-07 17:46:08 -0400

    Dashboard to detail

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router-deprecated';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -9,14 +10,18 @@ import { HeroService } from './hero.service';
 })
 export class DashboardComponent {
    heroes: Hero[] = [];
-   constructor(private heroService: HeroService) { }
+   constructor(
+      private router: Router,
+      private heroService: HeroService
+   ) { }
 
    ngOnInit() {
       this.heroService.getHeroes()
          .then(heroes => this.heroes = heroes.slice(1, 5));
    }
 
-   goToDetail() {
-      // TODO
+   goToDetail(hero: Hero) {
+      let link = ['HeroDetail', { id: hero.id }];
+      this.router.navigate(link);
    }
 }
